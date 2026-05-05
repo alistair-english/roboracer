c67e67d90fcbe0ab4a94e56c77660fcd4eef2211
Alistair English <hello@alistairenglish.com>
2026-05-05 10:01:14 +1000

    add breadcrumb and document autodrive interfaces

--- src/autodrive_roboracer/setup.py
+++ src/autodrive_roboracer/setup.py
@@ -14,6 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')), # Launch files
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')), # RViz configuration files
+        (os.path.join('share', package_name, 'interfaces'), glob('interfaces/*.yaml')), # jig/breadcrumb node interface definitions
     ],
     install_requires=['setuptools'],
     zip_safe=True,
